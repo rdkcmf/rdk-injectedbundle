514c530f8d84bf69bc8a3a0716bf7f1e2174f602
Alexandr Gavriliuc <agavriliuc@productengine.com>
2016-08-18 18:22:39 +0300

    WKIT-583 : Implement a log system in rdkbrowser2
    Reason for change: We need a log for use in rdkbrowser2
    Test Procedure: Execute rdkbrowser2 application and view the log file
    Risks: low
    Signed-off-by: Alexandr Gavriliuc <agavriliuc@productengine.com>
    
    Change-Id: I14b23a86c725604c649ac7fee25996ba6b7cb1e6

--- InjectedBundleMain.cpp
+++ InjectedBundleMain.cpp
@@ -3,8 +3,14 @@
 
 #include <WebKit/WKBundleInitialize.h>
 
+#include <stdlib.h>
+#include <stdio.h>
+
 extern "C" void WKBundleInitialize(WKBundleRef bundle, WKTypeRef initializationUserData)
 {
+    if (getenv("SYNC_STDOUT"))
+        setvbuf(stdout, NULL, _IOLBF, 0);
+
     JSBridge::initialize(bundle, initializationUserData);
     AVESupport::initialize();
 }
